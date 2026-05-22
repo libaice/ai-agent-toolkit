0bdb5e56b55c176c39fb260ae289bbdd2a176cf0
libaice <libaice147@gmail.com>
2026-05-22 16:25:27 +0800

    feat: implement tool use agent with get_price function

--- src/ai_agent_toolkit/__init__.py
+++ src/ai_agent_toolkit/__init__.py
@@ -5,15 +5,61 @@ from dotenv import load_dotenv
 load_dotenv(override=True)
 client = anthropic.Anthropic(api_key=os.getenv("ANTHROPIC_API_KEY"))
 
+messages = [{"role": "user", "content": "BTC 现在多少钱"}]
+
+
+tools = [
+    {
+        "name": "get_price",
+        "description": "获取某个资产的当前价格。当用户问价格时使用。",
+        "input_schema": {
+            "type": "object",
+            "properties": {
+                "symbol": {"type": "string", "description": "资产符号，比如 BTC、ETH"}
+            },
+            "required": ["symbol"],
+        },
+    }
+]
+
+
+def get_price(symbol: str) -> str:
+    prices = {"BTC": "77424", "ETH": "2128"}
+    return prices.get(symbol, "Not Found")
+
 
 def main() -> None:
     response = client.messages.create(
         model="claude-sonnet-4-20250514",
         max_tokens=1000,
-        system="你是一个交易助手",
-        messages=[
-            {"role": "user", "content": "BTC 最近走势怎么样"}
-        ]
+        tools=tools,
+        messages=messages,
     )
 
-    print(response)
+    if response.stop_reason == "tool_use":
+        tool_call = response.content[0]
+        tool_name = tool_call.name  # "get_price"
+        tool_args = tool_call.input  # {"symbol": "BTC"}
+        tool_id = tool_call.id  # "call_abc123"
+
+        # execute
+        result = get_price(**tool_args)
+        print("get the price from cex", result)
+
+        messages.append({"role": "assistant", "content": response.content})
+    messages.append(
+        {
+            "role": "user",
+            "content": [
+                {"type": "tool_result", "tool_use_id": tool_id, "content": result}
+            ],
+        }
+    )
+    final = client.messages.create(
+        model="claude-sonnet-4-20250514",
+        max_tokens=1000,
+        tools=tools,
+        messages=messages,
+    )
+
+    print(final.content[0].text)
